9c63f295072b1d11dd2d9380582048e07d721bf3
dkwardani28 <dkwardani28@gmail.com>
2022-11-11 21:34:10 +0700

    add test

---
+++ src/test/java/starter/Labels/LabelsAPI.java
@@ -0,0 +1,66 @@
+package starter.Labels;
+
+import io.restassured.http.ContentType;
+import net.serenitybdd.rest.SerenityRest;
+import net.thucydides.core.annotations.Step;
+
+import java.io.File;
+
+public class LabelsAPI {
+
+    public static final String URL = "https://api.todoist.com";
+    public static final String DIR = System.getProperty("user.dir");
+    public static final String JSON_REQ_BODY = DIR+"/src/test/resources/features/JSON/RequestBody";
+    public static final String JSON_SCHEMA = DIR+"/src/test/resources/features/JSON/Schema";
+
+    public static final String BEARER_TOKEN = "181d382f2da19201acc048d06cc5602ed7a15491";
+
+    public static final String GET_ALL_LABELS = URL+"/rest/v1/labels";
+    public static final String CREATE_NEW_LABEL = URL+"/rest/v1/labels/";
+    public static final String GET_LABEL = URL+"/rest/v1/labels/{id}";
+    public static final String UPDATE_LABEL = URL+"/rest/v1/labels/{id}";
+    public static final String DELETE_LABEL= URL+"/rest/v1/labels/{id}";
+
+    //Positive Case
+    // Get All Labels
+    @Step("Get all labels")
+    public void getAllLabels() {
+        SerenityRest.given()
+                .headers("Authorization", "Bearer "+BEARER_TOKEN);
+    }
+
+    //Create New Label
+    @Step("Post create new label")
+    public void postCreateNewLabel(File json){
+        SerenityRest.given()
+                .headers("Authorization", "Bearer "+BEARER_TOKEN)
+                .contentType(ContentType.JSON)
+                .body(json);
+    }
+
+    //Get Label
+    @Step("Get label")
+    public void getLabel(long id) {
+        SerenityRest.given()
+                .headers("Authorization", "Bearer "+BEARER_TOKEN)
+                .pathParam("id",id);
+    }
+
+    @Step("Update lael")
+    public void updateLabel(long id, File json){
+        SerenityRest.given()
+                .headers("Authorization", "Bearer "+BEARER_TOKEN)
+                .pathParam("id",id)
+                .contentType(ContentType.JSON)
+                .body(json);
+    }
+
+    //Delete Label
+    @Step("Delete label")
+    public void deleteLabel(long id) {
+        SerenityRest.given()
+                .headers("Authorization", "Bearer "+BEARER_TOKEN)
+                .pathParam("id",id);
+    }
+
+}
---
+++ src/test/resources/features/Labels/CreateNewLabel.feature
@@ -0,0 +1,10 @@
+Feature: Create New Label Todoist
+
+  @Labels @PositiveCase
+  Scenario: Post create new label todoist with json
+    Given Post create a valid new label
+    When Send post create label request
+    Then Status code is 200 OK
+    And Response body should contain name "Junkfood"
+    And Validate create label json schema validator
+
